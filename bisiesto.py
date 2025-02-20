ae7fe639a85e4366aa1cfc45e363c422b2f55636
Noreth29 <noreth.guajardo@iteso.mx>
2025-02-19 18:52:20 -0600

    intento 3

--- bisiesto.py
+++ bisiesto.py
@@ -9,15 +9,16 @@ Inserta el encabezado aquí y escribe tu código abajo
 
 año = int(input("Introduzca un año: "))
 
+
 # Proceso
 
-if año % 4 == 0 and año % 400 == 0:
-    salida = str(año) + " sí es un año bisiesto"
+if (año % 400 == 0 and año % 100 == 0) or (año % 4 == 0 and año % 100 != 0):
+    salida = str(año) + " sí es un año bisiesto" 
 
 else:
     salida = str(año) + " no es un año bisiesto"
 
 
-
 # Salidas
 print(salida)
+
